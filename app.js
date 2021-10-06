a5e9da8fd97905e7633970f79be2a95d375a1b6e
andhika16 <moh.andhika16@gmail.com>
2021-10-06 09:45:43 +0700

    perubahan 2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const expresslayout = require('express-ejs-layouts');
 app.listen(PORT, () => {
     console.log(`server running port : ${PORT}`);
