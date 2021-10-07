ec37bd0e198d1a1043e38a1fca7e74532d91c5f6
andhika16 <moh.andhika16@gmail.com>
2021-10-07 09:18:01 +0700

    update

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const expresslayout = require('express-ejs-layouts');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
+server.use(middlewares);
+server.use(router);
 
 
 
